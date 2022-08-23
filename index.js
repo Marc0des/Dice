2ba57c4109ffaa7265d233bab3d73ced06f02f5f
Marco Polo <107538690+Marc0des@users.noreply.github.com>
2022-08-24 03:36:27 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+var button = document.querySelector(".btn");
+button.addEventListener("click", start);
+
+function start() {
+ 
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var imgsource = "/images/dice" + randomNumber1 + ".png" ;
+
+var img1 = document.querySelectorAll("img")[0].setAttribute("src", imgsource);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var imgsource2 = "/images/dice" + randomNumber2  + ".png";
+
+var img2 = document.querySelectorAll("img")[1].setAttribute("src", imgsource2);
+
+
+/* resulta to bich*/
+if ( randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "Draw!";
+} else if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins";
+} else{
+    document.querySelector("h1").innerHTML = "Player 2 Wins 🚩";
+}
+
+}
